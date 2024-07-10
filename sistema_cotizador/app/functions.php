91fe93250d3a0eba5b0414ab26c1d5e035e2f44c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-07-09 20:12:33 -0500

    Creando la función recalculate_quote para hacer los calculos necesarios para cada objeto que se quiera calcular

--- sistema_cotizador/app/functions.php
+++ sistema_cotizador/app/functions.php
@@ -28,3 +28,36 @@ function get_quote(){
 
     return $_SESSION['new_quote'];
 }
+
+function recalculate_quote(){
+    $items = [];
+    $subtotal = 0;
+    $taxes = 0;
+    $shipping = 0;
+    $total = 0;
+
+    if(!isset($_SESSION['new_quote'])){
+        return false;
+    }
+
+    // validar items
+    $items = $_SESSION['new_quote']['items'];
+
+    // Si la lista de items está vacia no es necesario calcular nada 
+    if(!empty($items)){
+        foreach($items as $item){
+            $subtotal += $item['total'];
+            $taxes += $item['taxes'];
+        }
+    }
+
+    $shipping = $_SESSION['new_quote']['shipping'];
+    $total = $subtotal + $taxes + $shipping;
+
+    $_SESSION['new_quote']['subtotal'] = $subtotal;
+    $_SESSION['new_quote']['taxes'] = $taxes;
+    $_SESSION['new_quote']['shipping'] = $shipping;
+    $_SESSION['new_quote']['total'] = $total;
+
+    return true;
+}
