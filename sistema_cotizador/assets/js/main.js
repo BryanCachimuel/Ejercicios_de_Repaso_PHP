9572e9538e817fbabccfe28ac28c422b0580608e
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-07-11 21:49:33 -0500

    Usando ajax para cargar mediante una función el contenido de la cotización

--- sistema_cotizador/assets/js/main.js
+++ sistema_cotizador/assets/js/main.js
@@ -1,1 +1,32 @@
-$('body').waitMe();
+$("document").ready(() => {
+  /* Cargar contenido de la cotización */
+  function get_quote() {
+    let wrapper = $(".wrapper_quote");
+    action = "get_quote_res";
+
+    $.ajax({
+      url: "ajax.php",
+      type: "get",
+      cache: false,
+      dataType: "json",
+      data: { action },
+      beforeSend: function () {
+        wrapper.waiMe();
+      },
+    })
+      .done((res) => {
+        if(res.status === 200){
+
+        }else{
+            wrapper.html(res.msg);
+        }
+      })
+      .fail((err) => {
+        wrapper.html('Ocurrio un error, recarga la p{agina...');
+      }).always(() => {
+        wrapper.waiMe('hide');
+      });
+  }
+
+  get_quote();
+});
