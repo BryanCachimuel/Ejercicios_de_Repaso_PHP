b7933074d6b185591200712d0ff7e952911adda9
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-01 21:26:52 -0500

    Mando a llamar a los archivos de la conexión hacia la base de datos y del controlador del proyecto

---
+++ arquitectura_web_profesional/index.php
@@ -0,0 +1,6 @@
+<?php
+/*TODO: 6. Se manda a llamar al archivo con la conexión hacia la base de datos
+           y al archivo del controlador
+*/
+require_once("db/db.php");
+require_once("controlador/libros.php");
