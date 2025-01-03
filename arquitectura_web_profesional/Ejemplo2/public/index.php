3eb089441e50c69d0f109efdec4dd0d40992b90b
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-02 20:57:00 -0500

    Mandando a llamar una sola vez al archivo inicio.php como también se crea una instancia de la clase Control

---
+++ arquitectura_web_profesional/Ejemplo2/public/index.php
@@ -0,0 +1,6 @@
+<?php 
+
+/* El archivo .htaccess -> sirve como seguridad para que no se tenga acceso a los directorios y archivos del proyecto */
+
+require_once("../app/inicio.php");
+$control = new Control();
