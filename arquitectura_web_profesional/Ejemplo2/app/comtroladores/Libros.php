791d28b0c84a527e522d22a14869f5a2f08876ae
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-02 20:31:00 -0500

    Creando la clase Libros que extiende de la clase Controlador en la cual se codifica una variable una función constructora y una función index

---
+++ arquitectura_web_profesional/Ejemplo2/app/comtroladores/Libros.php
@@ -0,0 +1,17 @@
+<?php
+
+class Libros extends Controlador {
+
+    private $modelo;
+
+    function __construct(){
+        $this->modelo = $this->modelo("LibrosModelo");
+    }
+
+    public function index(){
+        $data = $this->modelo->getLibros();
+        // llamamos a la vista
+        $this->vista("LibrosVista",$data);
+    }
+
+}
